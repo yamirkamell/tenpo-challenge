9518ba44b5ac440a7c4f908208dddaa2f08c1d20
yamir kamell <yamirkamell@gmail.com>
2025-04-14 18:54:55 -0500

    delete util functions

--- src/utils/getCookie.util.ts
+++
@@ -1,5 +0,0 @@
-export const getCookie = (name: string): string | undefined => {
-    const value: string = `; ${document.cookie}`;
-    const parts: string[] = value.split(`; ${name}`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift();
-};
